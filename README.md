42fef5602aaf7f76dca9c9211a2d82eca39f52c1
kraujinis <kraujinis@gmail.com>
2023-06-17 17:52:02 +0300

    finished connect_to_rpi.py file

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ It's Python project for learn OOP. Everything run in Terminal.
 - You can customise menu before pay.
 
 ## How run a program
-First need declarate some values.
-- "it will be later"
+- file connect_to_rpi.py
+First need declare some values.
+- 
+
+***
+
+
+
--- connect/connect_to_rpi.py
+++ connect/connect_to_rpi.py
@@ -1,109 +1,141 @@
 from pymongo import MongoClient
 from dataclasses import dataclass
 import json
+from typing import Any, MutableMapping
 from pymongo.errors import PyMongoError, ConnectionFailure
-
-
-@dataclass
-class MainRpiRemoteDb:
-    user_name: str
-    user_passwd: str
-    host: str
-    port: int
-    db_name: str
-    collection: str
+from urllib.parse import quote_plus  # kodavimas
 
 
 class ConnectToMongoWithConfig:
-    def __init__(self, config: json, db_name: str, collection_name: str) -> None:
-        self.config = config
-        self.db_name = db_name
-        self.collection_name = collection_name
+    """class for making some manipulations with mongodb.
+    first need push the configuration file in JSON format:
+        {
+            "user_name": "user_name",
+            "user_passwd": "strong password",
+            "host": "192.168.1.81" (ip_address)
+            "port": "27017" (port)
+        }
+    JSON file can be stored where you want, just give a Relative Path:
+        config_file = (
+        "/home/user/Documents/code_academy_projects/cafeteria/connect/config.json"
+    )"""
 
-    def connect_to_mongodb(self) -> MongoClient:
-        """prisijungiama prie RPI"""
-        try:
-            with open(self.config, "r") as f:
-                config = json.load(f)
-
-            username = config.get("user_name")
-            password = config.get("user_psswd")
-            host = config.get("host")
-            port = config.get("port")
-
-            db = MainRpiRemoteDb(
-                user_name=username,
-                user_passwd=password,
-                host=host,
-                port=port,
-                db_name=self.db_name,
-                collection=self.collection_name,
-            )
-            return db
-
-        except ConnectionFailure as e:
-            print("Connection failure:", str(e))
-            return None
-        except PyMongoError as e:
-            print("An error occurred:", str(e))
-            return None
-
-
-class CreateValidationSchema(ConnectToMongoWithConfig):
-    def __init__(self, base: MainRpiRemoteDb, collection_name: str):
-        self.collection_name = collection_name
-        uri = "mongodb://%s:%s@%s:%s/" % (
-            base.user_name,
-            base.user_passwd,
-            base.host,
-            base.port,
-        )
-        self.db_name = base.db_name
-        self.client = MongoClient(uri)
-        self.db = self.client[base.db_name]
-        self.collection = self.db[collection_name]
-
-    def create_unique_index(
-        self, unique_field_name: str, number: int, unique_bool: bool
-    ) -> None:
-        self.collection.create_index([(unique_field_name, number)], unique=unique_bool)
-
-    def define_schema_validation_rules(self, schema_json: json):
-        with open(schema_json, "r") as f:
+    def __init__(self, config: Any) -> None:
+        with open(config, "r") as f:
             config = json.load(f)
 
-        self.db.command("collMod", self.collection.name, config)
+        username = config.get("user_name")
+        password = config.get("user_psswd")
+        host = config.get("host")
+        port = config.get("port")
+        uri = "mongodb://%s:%s@%s:%s/" % (
+            quote_plus(username),
+            quote_plus(password),
+            quote_plus(host),
+            port,
+        )
+        self.client = MongoClient(uri)
 
-    def drop_collection(self) -> bool:
-        try:
-            self.db.drop_collection(self.collection_name)
-            return True
-        except:
-            return False
-
-    def drop_database(self, db_name) -> bool:
+    def drop_database(self, db_name: str) -> str:
+        """function for drop database
+        just add in function database name (db_name)"""
         try:
             self.client.drop_database(db_name)
-            return True
-        except:
-            return False
+            return f"Database dropped successfully!"
+        except ConnectionFailure as e:
+            return f"Connection failure: {str(e)}"
+        except PyMongoError as e:
+            return f"An error occurred: {str(e)}"
+
+    def drop_collection(self, db_name: str, collection_name: str) -> str:
+        """function for drop collection
+        just add in the function collection name (db_name) and
+        collection name (collection_name)"""
+        try:
+            data_base = self.client[db_name]
+            data_base.drop_collection(collection_name)
+            return f"Collection dropped successfully!"
+        except ConnectionFailure as e:
+            return f"Connection failure: {str(e)}"
+        except PyMongoError as e:
+            return f"An error occurred: {str(e)}"
+
+    def define_schema_validation_rules(
+        self, schema_json: Any, db_name: str, collection_name: str
+    ) -> str:
+        """function for define schema validation rules.
+            JSON file can be stored where you want, just give a Relative Path:
+            config_file = (
+            "/home/user/Documents/code_academy_projects/cafeteria/connect/schema_json.json"
+        )
+        """
+        with open(schema_json, "r") as f:
+            schema_json = json.load(f)
+        data_base = self.client[db_name]
+        collection = data_base[collection_name]
+        data_base.command("collMod", collection.name, schema_json)
+        return f"Schema validation rules was successfully applied!"
+
+    def create_unique_index(
+        self,
+        unique_field_name: str,
+        number: int,
+        unique_bool: bool,
+        db_name: str,
+        collection_name: str,
+    ) -> MutableMapping:
+        """function to create a unique index
+        in function must to be given a unique field name,
+        number and boolean=True or False also database name
+        and collection name
+        """
+        data_base = self.client[db_name]
+        collection = data_base[collection_name]
+        collection.create_index([(unique_field_name, number)], unique=unique_bool)
+        return collection.index_information()
 
 
 if __name__ == "__main__":
     config_file = (
         "/home/giedrius/Documents/code_academy_projects/cafeteria/connect/config.json"
     )
-    db = ConnectToMongoWithConfig(
-        config=config_file, db_name="animals", collection_name="dogs"
-    ).connect_to_mongodb()
-    schema = CreateValidationSchema(base=db, collection_name="cafeteria")
+    db = ConnectToMongoWithConfig(config_file)
 
-    # schema.create_unique_index(
-    #     unique_field_name="customer_phone", number=1, unique_bool=False
+    print(db.drop_collection(db_name="test_db", collection_name="test"))
+
+    print(db.drop_database(db_name="test_db"))
+
+    # print(
+    #     db.create_unique_index(
+    #         unique_field_name="price",
+    #         number=1,
+    #         unique_bool=True,
+    #         db_name="pets",
+    #         collection_name="new_party",
+    #     )
     # )
+    # print(db.drop_database(db_name="exercise_db"))
+    # print(db.drop_collection(db_name="pets", collection_name="error"))
+    # print(
+    #     db.create_unique_index(
+    #         unique_field_name="name",
+    #         number=1,
+    #         unique_bool=True,
+    #         db_name="pets",
+    #         collection_name="new_party",
+    #     )
+    # )
+    # db = ConnectToMongoWithConfig(
+    #     config=config_file, db_name="animals", collection_name="dogs"
+    # ).connect_to_mongodb()
+    # schema = CreateValidationSchema(base=db, collection_name="cafeteria")
 
-    # customer_config = "/home/giedrius/Documents/code_academy_projects/cafeteria/validation_schema/customer.json"
-    # print(schema.define_schema_validation_rules(customer_config))
+    # # schema.create_unique_index(
+    # #     unique_field_name="customer_phone", number=1, unique_bool=False
+    # # )
 
-    # schema.drop_collection()
-    schema.drop_database("animals")
+    # # customer_config = "/home/giedrius/Documents/code_academy_projects/cafeteria/validation_schema/customer.json"
+    # # print(schema.define_schema_validation_rules(customer_config))
+
+    # # schema.drop_collection()
+    # schema.drop_database("animals")
